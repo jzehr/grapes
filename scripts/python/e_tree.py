766f1c51aa81b4be0af985d269da3558e8a70c1d
Jordan Zehr <jordan.zehr@temple.edu>
2019-07-26 19:22:30 -0400

    updated the 2 python files and the 1 bash file. The pipeline runs from start to finish and creates a treefile from iqtree

--- scripts/python/e_tree.py
+++ scripts/python/e_tree.py
@@ -4,7 +4,8 @@ import json
 
 #gpgv_file = 'GPGV_sequence.gbc.xml'
 #gva_file = 'GVA_sequence.gbc.xml'
-glrav3_file = 'GLRaV3_sequence.gbc.xml'
+front = '../../data/'
+glrav3_file = front + 'GLRaV3/NCBI_data/GLRaV3_sequence.gbc.xml'
 tree = et.parse(glrav3_file)
 
 '''
@@ -17,14 +18,7 @@ cousins are: INSDAuthor, INSDInterval
 cats are: INSDInterval_from, INSDInterval_to, INSDInterval_accession, INSDQualifier_name, INSDQualifier_value 
 '''
 
-def get_sequence_children(elem):
-    def checker(lst):
-        lst = lst
-        if len(lst) == 0:
-            lst.append('no_value')
-        return lst
-
-
+def get_info(elem):
     acc_num = []
     source = []
     seq = []
@@ -33,43 +27,43 @@ def get_sequence_children(elem):
     source.append(elem.find("INSDSeq_source").text)
     seq.append(elem.find("INSDSeq_sequence").text)
 
-    temp_country = list(elem.iter('INSDQualifier'))
-    f_temp_country = filter(lambda x: x.find('INSDQualifier_name').text == 'country', temp_country)
-    m_temp_country = list(map(lambda x: x.find('INSDQualifier_value').text, f_temp_country))
-    country = checker(m_temp_country)
+    def checker(lst):
+        temp = lst
+        if len(temp) == 0:
+            temp.append('no_value')
+        return temp
+    
+    def get_kids(word):
+        temp = list(elem.iter('INSDQualifier'))
+        temp_word = filter(lambda x: x.find('INSDQualifier_name').text == word, temp)
+        new_word = list(map(lambda x: x.find('INSDQualifier_value').text, temp_word))
+        return new_word
+    
 
-    temp_products = list(elem.iter('INSDQualifier'))
-    f_temp_products = filter(lambda x: x.find('INSDQualifier_name').text == 'product', temp_products)
-    m_temp_products = list(map(lambda x: x.find('INSDQualifier_value').text, f_temp_products))
-    product = checker(m_temp_products)
+    country = checker(get_kids('country'))
+    product = checker(get_kids('product'))
+    pro_id = checker(get_kids('protein_id'))
+    trans = checker(get_kids('translation'))
+    host = checker(get_kids('host'))
+    date = checker(get_kids('collection_date'))
 
-    temp_pro_id = list(elem.iter('INSDQualifier'))
-    f_temp_pro_id = filter(lambda x: x.find('INSDQualifier_name').text == 'protein_id', temp_pro_id)
-    m_temp_pro_id = list(map(lambda x: x.find('INSDQualifier_value').text, f_temp_pro_id))
-    pro_id = checker(m_temp_pro_id)
-
-    temp_trans = list(elem.iter('INSDQualifier'))
-    f_temp_trans = filter(lambda x: x.find('INSDQualifier_name').text == 'translation', temp_trans)
-    m_temp_trans = list(map(lambda x: x.find('INSDQualifier_value').text, f_temp_trans))
-    trans = checker(m_temp_trans)
-
-    #print(len(acc_num), len(source), len(seq), len(country), len(product), len(pro_id), len(trans))
-    #print(acc_num, source, seq, m_temp_country, m_temp_products)
-    total_info = list(zip(acc_num, source, country, product, pro_id, trans, seq))
+    #print(acc_num, source, seq, country, product, pro_id, trans, host, date, '\n')
+    
+    total_info = list(zip(acc_num, source, seq, country, product, pro_id, trans, host, date))
     return total_info
     
 
 
 root = tree.getroot()
-all_info = list(map(get_sequence_children, root.findall("INSDSeq")))
-#print(all_info)
+all_info = list(map(get_info, root.findall("INSDSeq")))
+
 
 my_dict = {}
-for i in all_info:
+for info in all_info:
     temp = {}
-    for l in i:
+    for i in info:
         temp = {}
-        temp[l[0]] = {'source': l[1], 'country': l[2], 'products': l[3], 'protein_id': l[4], 'translation_of_protein': l[5], 'seq': l[6]}
+        temp[i[0]] = {'source': i[1], 'seq': i[2], 'country': i[3], 'product': i[4], 'host': i[7], 'date': i[8]}
         my_dict.update(temp)
 #print(my_dict)
 
@@ -77,11 +71,10 @@ for i in all_info:
 data = json.dumps(my_dict)
 #print(data)
 
-data_file = 'GLRaV3_sequence.json'
+prefix = '../../data/'
+data_file = prefix + 'GLRaV3/NCBI_data/GLRaV3_sequence.json'
 with open(data_file, 'w') as out_file:
     out_file.write(data)
-
-
 
 
 
